363748103a439c4b474a40b28493fa624df89e5f
Dawid Iwaniuk <dawuso@gmail.com>
2022-01-19 22:04:36 +0100

    Naprawa błedów

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React  from 'react' 
-import {BrowserRouter,Route, Routes} from 'react-router-dom'
-import SignUp from './Pages/SignUp'
-import Login from './Pages/Login'
-import ResetPassword from './Pages/ResetPassword'
-
-
-function App () {
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignUp from "./Pages/SignUp";
+import Login from "./Pages/Login";
+import ResetPassword from "./Pages/ResetPassword";
+function App() {
   return (
     <div>
-      
       <BrowserRouter>
-      <Routes>
-        <Route exact="exact" path="/" component={SignUp}/>
-        <Route  path="/login" component={Login}/>
-        <Route  path="/reset" component={ResetPassword}/>
-
-      </Routes>
+        <Routes>
+          <Route exact="exact" path="/" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/reset" element={<ResetPassword />} />
+        </Routes>
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
 export default App;
