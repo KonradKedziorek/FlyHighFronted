acf98dbef40b07dec834e9fcf9ed57fb813bcbc0
justyna2104 <justynaharmuszkiewicz@gmail.com>
2022-03-21 22:53:39 +0100

    Connected back to front EditPersonalData

--- src/components/Pages/EditPersonalDataPage.js
+++ src/components/Pages/EditPersonalDataPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../../App.css';
 import EditPersonalData from '../EditPersonalData'
 import Footer from '../Footer';
@@ -6,6 +6,46 @@ import Navbar from '../Navbar';
 
 
 function EditPersonalDataPage() {
+
+  const [name, setname] = useState("");
+    const [middleName, setmiddleName] = useState("");
+    const [surname, setsurname] = useState("");
+    const [country, setcountry] = useState("");
+    const [city, setcity] = useState("");
+    const [street, setstreet] = useState("");
+    const [email, setemail] = useState("");
+    const [bankAccount, setbankAccount] = useState("");
+    const [phoneNumber, setphoneNumber] = useState("");
+    const [houseNumber, sethouseNumber] = useState("");
+    const [zipCode, setzipCode] = useState("");
+
+   useEffect(() => {
+    const EditPD = {name,middleName,surname,country,city,street,email,phoneNumber,houseNumber,zipCode,bankAccount}
+    let item = localStorage.getItem("TOKEN_KEY");
+    let token = "Bearer " + item 
+    fetch("http://localhost:8080/EditPersonalData", {
+      method: "GET",
+      headers: {"Content-Type": "application/json", "Authorization": token}
+    }).then((response) => response.json()
+    ).then(json => {
+      localStorage.setItem("personal_Data", JSON.stringify(json))
+        setname(json.name)
+        setmiddleName(json.middleName)
+        setsurname(json.surname)
+        setcountry(json.country)
+        setcity(json.city)
+        setstreet(json.street)
+        setemail(json.email)
+        setphoneNumber(json.phoneNumber)
+        sethouseNumber(json.houseNumber)
+        setzipCode(json.zipCode)
+        setbankAccount(json.bankAccount)
+      })
+      .finally( () => {
+        console.log("Dane: " + EditPD)
+      })
+   },[])
+
   return (
     <>
     {<Navbar/>}
